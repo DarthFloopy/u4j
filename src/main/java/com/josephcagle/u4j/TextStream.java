9b9a156cea96d8139e4a17e9de03edc0fd036843
Joseph Cagle <joseph@josephcagle.com>
2021-04-12 19:42:53 -0400

    Add lots of documentation in TextStream

--- src/main/java/com/josephcagle/u4j/TextStream.java
+++ src/main/java/com/josephcagle/u4j/TextStream.java
@@ -12,11 +12,22 @@ import java.util.stream.Stream;
 
 /**
  * A TextStream represents an immutable, finite stream of lines of text. You
- * can transform TextStreams to process data.
+ * can transform TextStreams to process text data.
+ *
+ * @author Joseph Cagle
  */
 public class TextStream {
     private final List<String> lines;
 
+    /**
+     * The constructor takes a {@link List} of lines of text. To create a
+     * {@link TextStream} based on other types of sources, such as single lines or
+     * whole files, use methods in {@link TextStreamProducers}. (E.g.,
+     * {@link TextStreamProducers#echo(String)} gives you a TextStream from a
+     * single line.)
+     *
+     * @param lines A {@link List} of the lines of text for this {@link TextStream}.
+     */
     public TextStream(List<String> lines) {
         if (lines == null)
             throw new IllegalArgumentException("lines must not be null");
@@ -30,9 +41,10 @@ public class TextStream {
 
     /**
      *
-     * Compares the specified object with this TextStream for equality. Returns
-     * true if and only if the specified object is {@code instanceof}
-     * {@link TextStream} and it contains the same lines of text.
+     * Compares the specified object with this TextStream for equality.
+     *
+     * @return true if and only if the specified object is {@code instanceof}
+     *         {@link TextStream} and it contains the same lines of text.
      */
     @Override
     public final boolean equals(Object that) {
@@ -48,11 +60,23 @@ public class TextStream {
         return this.lines.equals(tsThat.lines);
     }
 
+    /**
+     * Implements a hash code method in accordance with the equals() contract.
+     *
+     * @see Object#hashCode()
+     * @see Object#equals(Object)
+     */
     @Override
     public int hashCode() {
         return this.lines.hashCode();
     }
 
+    /**
+     * Returns the text data as a {@link String}.
+     *
+     * @return this {@link TextStream}'s lines of text, separated by
+     *         {@link System#lineSeparator()}.
+     */
     @Override
     public final String toString() {
         return lines.stream().collect(joining(System.lineSeparator()));
@@ -88,6 +112,15 @@ public class TextStream {
      * TODO: Add links to manpages for each emulated Unix tool
      */
 
+    /**
+     * Gets the first {@code numLines} lines of text.
+     *
+     * @param numLines the number of lines to take from the beginning
+     * @return a new {@link TextStream} of the first {@code numLines} lines of this
+     *         {@link TextStream}
+     * @throws IllegalArgumentException if {@code lines} < 0 or if {@code lines} >
+     *                                  {@link #length()}
+     */
     public final TextStream head(int numLines) {
         if (numLines < 0)
             throw new IllegalArgumentException("numLines must be >= 0");
@@ -96,8 +129,23 @@ public class TextStream {
 
         return new TextStream(this.lines.subList(0, numLines));
     }
+
+    /**
+     * A convenience method, which calls {@link #head(int)} with an argument of 10.
+     *
+     * @return {@code head(10)}
+     */
     public final TextStream head() { return head(10); }
 
+    /**
+     * Gets the last {@code numLines} lines of text.
+     *
+     * @param numLines the number of lines to take from the end
+     * @return a new {@link TextStream} of the last {@code numLines} lines of this
+     *         {@link TextStream}
+     * @throws IllegalArgumentException if {@code lines} < 0 or if {@code lines} >
+     *                                  {@link #length()}
+     */
     public final TextStream tail(int numLines) {
         if (numLines < 0)
             throw new IllegalArgumentException("numLines must be >= 0");
@@ -109,6 +157,14 @@ public class TextStream {
         );
     }
 
+    /**
+     * Gets all lines containing at least one match for a regex.
+     *
+     * @param regex A string containing the regex to search for
+     * @return A new {@link TextStream} of all lines containing a match for
+     *         {@code regex}
+     * @throws PatternSyntaxException if {@code regex} is invalid
+     */
     public final TextStream grep(String regex) {
         if (regex == null)
             throw new IllegalArgumentException("regex must not be null");
@@ -128,6 +184,9 @@ public class TextStream {
         );
     }
 
+    /**
+     * Prints all lines of this {@link TextStream} to System.out.
+     */
     public final void cat() {
         for (String line : lines)
             System.out.println(line);
